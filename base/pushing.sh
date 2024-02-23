96cc3e83bec1bf53d77444cd26b6d271bcd259f1
evilTachyMeter <daniel.mock42@gmail.com>
2024-02-22 19:17:26 -0700

    chinese mealboxes

--- base/pushing.sh
+++ base/pushing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFAULT_PUSH_COMMENT="chinese mealboxes"
+DEFAULT_PUSH_COMMENT="meat "
 
 git add --all
 git commit -m "${*:-$DEFAULT_PUSH_COMMENT}"
--- config/pushing.sh
+++ config/pushing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFAULT_PUSH_COMMENT="chinese mealboxes"
+DEFAULT_PUSH_COMMENT="rice balls"
 
 git add --all
 git commit -m "${*:-$DEFAULT_PUSH_COMMENT}"
